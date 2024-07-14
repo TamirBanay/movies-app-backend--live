e8e706307aae013b4ba8e4b5549b74cc342c7dfd
Tamir Banay <banay9329@gmail.com>
2024-07-14 17:03:37 +0300

    Update Procfile and settings for Heroku deployment

--- movies_app/wsgi.py
+++ movies_app/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for movies_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# movies_app/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movies_app.settings')
