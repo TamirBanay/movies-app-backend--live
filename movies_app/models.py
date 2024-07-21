26cf16ff6138468e146db309067143863648a3fb
Tamir Banay <banay9329@gmail.com>
2024-07-21 14:50:41 +0300

    Prepare for deployment to Heroku

--- movies_app/models.py
+++ movies_app/models.py
@@ -14,8 +14,6 @@ class Favorite(models.Model):
 class Favorite_series(models.Model):
     tmdb_series_id = models.IntegerField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+
     class Meta:
-      unique_together = ('tmdb_series_id', 'user',)
-      
-      
+        unique_together = ('tmdb_series_id', 'user',)
